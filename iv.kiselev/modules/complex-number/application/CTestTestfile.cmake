349d5abc9594c39b52e7260c9fe299e0d0bd1d25
Umlore1998 <iv.kiselev1998@yandex.ru>
2017-04-14 21:46:16 +0300

    CMake

---
+++ iv.kiselev/modules/complex-number/application/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/PROGRAMS/devtools-course-practice/modules/complex-number/application
+# Build directory: D:/PROGRAMS/devtools-course-practice/iv.kiselev/modules/complex-number/application
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ iv.kiselev/modules/complex-number/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/PROGRAMS/devtools-course-practice/modules/complex-number/src
+# Build directory: D:/PROGRAMS/devtools-course-practice/iv.kiselev/modules/complex-number/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
