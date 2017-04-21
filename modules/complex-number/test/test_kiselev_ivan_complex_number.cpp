ed3a1f0ca5cac4eb03706ba61dd40821644b5e48
Umlore1998 <iv.kiselev1998@yandex.ru>
2017-04-21 12:37:47 +0300

    It's All

--- modules/complex-number/test/test_kiselev_ivan_complex_number.cpp
+++ modules/complex-number/test/test_kiselev_ivan_complex_number.cpp
@@ -16,3 +16,42 @@ TEST(Kornyakov_Kirill_ComplexNumberTest, Can_Create_Zero) {
     EXPECT_EQ(re, z.getRe());
     EXPECT_EQ(im, z.getIm());
 }
+
+TEST(Kiselev_complex_number_test, simple_test) {
+	//Act
+	ComplexNumber first(2.0, 3.0);
+	ComplexNumber second(2.0, 3.0);
+
+	//Assert
+	EXPECT_TRUE(first == second);
+}
+
+TEST(Kiselev_complex_number_test, Add_test) {
+	//Act
+	ComplexNumber first(3.0, 2.5);
+	ComplexNumber second(2.0, 3.0);
+	ComplexNumber result(1.0, -0.5);
+
+	//Assert
+	EXPECT_TRUE(result == first - second);
+}
+
+TEST(Kiselev_complex_number_test, multiplication_test) {
+	//Act
+	ComplexNumber first(3.0, 2.0);
+	ComplexNumber second(2.0, 3.0);
+	ComplexNumber result(0, 13.0);
+
+	//Assert
+	EXPECT_TRUE(result == first * second);
+}
+
+TEST(Kiselev_complex_number_test, division_test) {
+	//Act
+	ComplexNumber first(2.0, 2.0);
+	ComplexNumber second(2.0, 2.0);
+	ComplexNumber result(1.0, 0.0);
+
+	//Assert
+	EXPECT_TRUE(result == first / second);
+}
